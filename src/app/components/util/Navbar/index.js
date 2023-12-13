748d4c14b25a36bf9f174c1f377c1170a37f7fe5
rprasetya <rayaprasetya90@gmail.com>
2023-12-13 16:13:05 +0800

    setup next and homepage

---
+++ src/app/components/util/Navbar/index.js
@@ -0,0 +1,21 @@
+import Link from "next/link"
+
+const Navbar = () => {
+  return(
+    <header>
+      <div className="bg-sky-600 p-5 rounded-b-2xl flex justify-between items-center">
+        <div className="flex gap-5 ">
+          <Link href='/' className="bg-slate-200 py-2 px-4 rounded">Home</Link>
+          <Link href='/courses' className="py-2 px-4 rounded">Courses</Link>
+          <Link href='/about' className="py-2 px-4 rounded">About Us</Link>
+          <Link href='/contact' className="py-2 px-4 rounded">Contact</Link>
+        </div>
+        <div>
+          <Link href='/signup' className="py-2 px-4 rounded">Sign Up</Link>
+          <Link href='/login'className="py-2 px-4 rounded bg-black text-white">Login</Link>
+        </div>
+      </div>
+    </header>
+  )
+}
+export default Navbar
